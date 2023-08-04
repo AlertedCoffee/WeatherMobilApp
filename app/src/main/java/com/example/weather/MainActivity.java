08f258b3413ddfa339e9887a5d7bc87ad2bc7a7c
AlertedCoffee <ozolin.matvei@yandex.ru>
2023-08-04 18:03:11 +0300

    some working version without search

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.net.URL;
 import java.sql.Date;
@@ -22,13 +24,30 @@ import java.util.TimeZone;
 public class MainActivity extends AppCompatActivity implements GetWeather.AsyncResponses {
 
     private static final String TAG = "MainActivity";
-    
+
+    TextView townName;
+    TextView description;
+    TextView temp;
+    TextView feelsTemp;
+    TextView wind;
+    TextView sunrise;
+    TextView sunset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        townName = findViewById(R.id.townNameTextView);
+        description = findViewById(R.id.weatherValueTextView);
+        temp = findViewById(R.id.tempValueTextView);
+        feelsTemp = findViewById(R.id.feelsTempValueTextView);
+        wind = findViewById(R.id.windSpeedValueTextView);
+        sunrise = findViewById(R.id.sunriseValueTextView);
+        sunset = findViewById(R.id.sunsetValueTextView);
+
+
         try {
-            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=%D0%9A%D0%BE%D0%BF%D0%BE%D1%80%D1%8C%D0%B5&appid=f38e5ba35d6cb5b542711ce044c35e01&units=metric&lang=ru");
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Москва&appid=f38e5ba35d6cb5b542711ce044c35e01&units=metric&lang=ru");
             new GetWeather(this).execute(url);
         }
         catch (Exception e){
@@ -45,16 +64,25 @@ public class MainActivity extends AppCompatActivity implements GetWeather.AsyncR
 
             JSONObject weather = jsonArray.getJSONObject(0);
             JSONObject main = jsonResult.getJSONObject("main");
+            JSONObject jWind = jsonResult.getJSONObject("wind");
             JSONObject sys = jsonResult.getJSONObject("sys");
             String name = jsonResult.getString("name");
 
+            townName.setText(name);
+            description.setText(weather.getString("description"));
+            temp.setText(main.getString("temp"));
+            feelsTemp.setText(main.getString("feels_like"));
+            wind.setText(jWind.getString("speed") + " м/c");
+
+
             Locale locale = new Locale("ru", "RU");
             SimpleDateFormat format = new SimpleDateFormat("HH.mm.ss", locale);
-            Long timezone = Long.parseLong(jsonResult.getString("timezone")) * 1000;
-            String dateString = format.format(new Date(Long.parseLong(sys.getString("sunrise"))));
+            String dateString = format.format(new Date(Long.parseLong(sys.getString("sunrise")) * 1000));
+            sunrise.setText(dateString);
 
-            TextView textView = findViewById(R.id.textView);
-            textView.setText(dateString);
+            dateString = format.format(new Date(Long.parseLong(sys.getString("sunset")) * 1000));
+            sunset.setText(dateString);
+
 
         }
         catch (JSONException e){
